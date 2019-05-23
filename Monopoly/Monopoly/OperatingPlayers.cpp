d5a08eb55a53b2adfd8cd921807409347d2c5651
p458732 <p458732@kimo.com>
2019-05-23 13:42:57 +0800

    MAXPLATER

--- Monopoly/Monopoly/OperatingPlayers.cpp
+++ Monopoly/Monopoly/OperatingPlayers.cpp
@@ -1,6 +1,5 @@
 #include "OperatingPlayers.h"
-
-
+#include<time.h>
 
 OperatingPlayers::OperatingPlayers()
 {
@@ -10,34 +9,45 @@ OperatingPlayers::OperatingPlayers()
 
 void OperatingPlayers::GameStart()
 {
-	GameMap temp;
+	
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD Position;
 	Position.X = 15;
 	Position.Y = 10;
 	SetConsoleCursorPosition(hOut, Position);
-	wcout << L"現在輪到 "<< (turn % 4 )+1 <<L"玩家擲骰子，請按任意鍵擲骰子";
+
+	wcout << L"現在輪到 "<< (turn % TheMap.GetMaxPlayers())+1 <<L"玩家擲骰子，請按任意鍵擲骰子";
+	command = _getch();
 	while (command != EOF)
 	{
+		
+		srand(static_cast<int>(time(NULL)));
+		Position.X = 15;
+		Position.Y = 12;
+		SetConsoleCursorPosition(hOut, Position);
 		wcout << L"擲出的點數為 " << (rand() % 6) + 1 << L" 點";
-		if ((turn % 4) + 1 == 1)
+		if ((turn % TheMap.GetMaxPlayers()) + 1 == 1)
 		{
 
 		}
-		else if ((turn % 4) + 1 == 2)
+		else if ((turn % TheMap.GetMaxPlayers()) + 1 == 2)
 		{
 
 		}
-		else if ((turn % 4) + 1 == 3)
+		else if ((turn % TheMap.GetMaxPlayers()) + 1 == 3)
 		{
 
 		}
-		else if ((turn % 4) + 1 == 4)
+		else if ((turn % TheMap.GetMaxPlayers()) + 1 == 4)
 		{
 
 		}
 		turn++;
+		Position.X = 15;
+		Position.Y = 10;
+		SetConsoleCursorPosition(hOut, Position);
+		wcout << L"現在輪到 " << (turn % TheMap.GetMaxPlayers()) + 1 << L"玩家擲骰子，請按任意鍵擲骰子";
 		command = _getch();
 	}
 }

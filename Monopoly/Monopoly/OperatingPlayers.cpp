0af112999be736fac9dceaf87d349e3faf434956
p458732 <p458732@kimo.com>
2019-05-26 22:00:51 +0800

    BUG SHIT

--- Monopoly/Monopoly/OperatingPlayers.cpp
+++ Monopoly/Monopoly/OperatingPlayers.cpp
@@ -1,6 +1,6 @@
 #include "OperatingPlayers.h"
 #include<time.h>
-
+#include <Windows.h>
 bool isEstateOwned(int pos) {
 	for (auto p : TheMap.PlayerList) {
 		for (auto estate : p.OwnedProperties) {
@@ -18,7 +18,7 @@ OperatingPlayers::OperatingPlayers()
 }
 
 int OperatingPlayers::PurchaseLand(int ID, int pos)
-{
+{//75left 77right 13 enter 
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD Position;
@@ -26,14 +26,70 @@ int OperatingPlayers::PurchaseLand(int ID, int pos)
 	Position.Y = 20;
 	SetConsoleCursorPosition(hOut, Position);
 	wcout << ID+1 << L"目前在 "<< TheMap[pos].Name <<L" 位置";
-	return 1;
+	Position.X = 15;
+	Position.Y = 22;
+	SetConsoleCursorPosition(hOut, Position);
+	wcout << L" 這片土地目前沒有人購買 是否要購買?";
+	Position.X = 15;
+	Position.Y = 26;	
+	SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x5d);
+	SetConsoleCursorPosition(hOut, Position);
+	wcout << L"yes";
+	SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x60);
+	wcout << L" no";
+
+	char commandTemp = 2;
+	int rightOrLeft = 0;
+	while (commandTemp != 13)
+	{
+		
+		if (rightOrLeft == 0)
+		{
+			if (commandTemp == 77)
+			{
+				Position.X = 15;
+				Position.Y = 26;
+				SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x90);
+				SetConsoleCursorPosition(hOut, Position);
+				wcout << L"yes";
+				SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x5d);
+				wcout << L" no";
+			}
+			rightOrLeft = 1;
+		}
+		else if (rightOrLeft == 1)
+		{
+			if (commandTemp == 75)
+			{
+				Position.X = 15;
+				Position.Y = 26;
+				SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x5d);
+				SetConsoleCursorPosition(hOut, Position);
+				wcout << L"yes";
+				SetConsoleTextAttribute(hOut, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | 0x90);
+				wcout << L" no";
+			}
+			rightOrLeft = 0;
+		}
+		commandTemp = _getch();
+	}
+	if (rightOrLeft == 0)
+	{
+		return 1;
+	}
+	else if (rightOrLeft == 1)
+	{
+		return 0;
+	}
+	
+	
 }
 
 void OperatingPlayers::GameStart()
 {
 	int isFirst = 0;
 	
-	
+	char command;
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD Position;
@@ -43,6 +99,7 @@ void OperatingPlayers::GameStart()
 
 	wcout << L"現在輪到 " << TheMap.GetCurrentPlayer().ID + 1 << L"玩家擲骰子，請按任意鍵擲骰子";
 	command = _getch();
+	
 	while (command != EOF)
 	{
 		if (isFirst != 0)
@@ -63,7 +120,7 @@ void OperatingPlayers::GameStart()
 		
 			TheMap.PlayerList[TheMap.GetCurrentPlayer().ID].Position = TheMap.PlayerList[TheMap.GetCurrentPlayer().ID].Position + DicePoint;
 			TheMap.PlayerList[TheMap.GetCurrentPlayer().ID].Position = TheMap.PlayerList[TheMap.GetCurrentPlayer().ID].Position % 28;
-	
+			TheMap.RefreshPlayerLocation();
 			//命運
 			if (TheMap[TheMap.GetCurrentPlayer().Position].Type == LocType::Destiny)
 			{
@@ -131,14 +188,17 @@ void OperatingPlayers::GameStart()
 			
 		
 		
-	
-		wcout << L"      請按任意鍵繼續...";
-		TheMap.RefreshPlayerLocation();
+		
+		
 		TheMap.TurnNextRound();
 		command = _getch();
 	
 		Position.X = 15;
 		Position.Y = 12;
+		SetConsoleCursorPosition(hOut, Position);
+		wcout << L"                                               ";
+		Position.X = 15;
+		Position.Y = 20;
 		SetConsoleCursorPosition(hOut, Position);
 		wcout << L"                                               ";
 		isFirst = 1;
